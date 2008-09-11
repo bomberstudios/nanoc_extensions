c49cb61cdd705beb647e7d94e26ab78396db2f2c
Ale Muñoz <bomberstudios@gmail.com>
2008-09-11 12:58:08 +0200

    Updated gist plugin and documentation

--- plugins/gist.rb
+++ plugins/gist.rb
@@ -1,3 +1,4 @@
 def gist gist_id
+  "<style type=\"text/css\" media=\"screen\">.gist pre { padding: 1em; } .gist pre span { font: 12px Consolas, monospace; }</style>\n" +
   "<script src=\"http://gist.github.com/#{gist_id.to_s}.js\"></script>"
 end
